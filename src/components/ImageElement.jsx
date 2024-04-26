66cadb7649fffb84579838935db82332f0142caa
twozer00 <leobardo.21@outlook.es>
2024-04-26 04:52:22 -0700

    minor fixes

--- src/components/ImageElement.jsx
+++ src/components/ImageElement.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 export default function ImageElement({ origin, converted, loading }) {
     return (
-        <div className="flex flex-row justify-between w-full h-28 p-2">
+        <div className="flex flex-row justify-around items-start py-2">
             {origin && <ImageContainer image={origin} />}
-            {(converted) ? <ImageContainer image={converted} /> : <div role='img' className='max-w-[50%] w-full h-full flex flex-row justify-center items-center font-semibold text-white'>{loading ? "Loading" : "waiting for convert"}</div>}
+            {(converted) ?
+                <>
+                    <ImageContainer image={converted} download />
+
+                </> :
+                <div role='img' className='max-w-[50%] w-full h-full flex flex-row justify-center items-center font-semibold text-white'>{loading ? "Loading" : "waiting for convert"}</div>}
         </div>
     )
 }
@@ -14,11 +19,20 @@ const getSize = (size) => {
     const mb = kb / 1024
     return (mb > 1) ? `${mb.toFixed(2)} mb` : `${kb.toFixed(2)} kb`
 }
-const ImageContainer = ({ image }) => {
+const ImageContainer = ({ image, download }) => {
     return (
-        <div className='flex flex-col items-center h-full w-full'>
-            <img src={URL.createObjectURL(image)} alt="origin" className='max-w-[50%] w-full object-contain h-full' />
-            <p className='text-white text-center'>{getSize(image.size)}</p>
+        <div className='flex flex-col w-full'>
+            <a download={download} href={URL.createObjectURL(image)} className='flex-auto h-32 w-full pointer-events-none inline-block' >
+                <img src={URL.createObjectURL(image)} alt="origin" className='w-full h-full object-center inline-block object-contain' />
+            </a>
+            <div className='h-fit'>
+                <p className='text-white text-center'>size: {getSize(image.size)}</p>
+                {!download && <p className='text-white text-center'>file name: {image.name}</p>}
+                {download && <a download={download} href={URL.createObjectURL(image)} className='w-fit text-white underline'>download</a>}
+            </div>
         </div>
     )
+}
+const handleDownload = (e) => {
+    e.target.closest('div').querySelector('a').click()
 }
--- src/pages/Init.jsx
+++ src/pages/Init.jsx
@@ -68,38 +68,40 @@ export default function Init() {
   }
 
   return (
-    <div className='flex flex-col h-screen bg-gradient-to-b from-sky-400 to-sky-800'>
-      <header className='sticky text-white h-12 flex flex-row justify-center items-center bg-white/30 backdrop-blur'>
-        <Link href='/' className='text-white font-bold text-2xl'>
-          Image to WEBP
-        </Link>
-      </header>
-      <main className='flex-1 h-full flex flex-col items-center justify-center max-w-xl mx-auto w-full gap-1'>
-        <h1 className='text-4xl font-bold text-center text-white'>Image to WEBP</h1>
-        <h2 className='text-center text-gray-100'>Convert your image to webp format</h2>
-        <section className='flex flex-col gap-2 w-full max-w-xl'>
-          <div className='w-full flex flex-row gap-2 flex-0'>
-            <div className='w-full h-10 bg-white/20 backdrop-blur shadow-lg rounded-full px-2 py-1 flex flex-row gap-2 items-center'>
-              <p className='block w-full text-white italic pl-2 select-none pointer-events-none'>{file && file[0]?.name || 'Filename'}</p>
-            </div>
-            <>
-              <label htmlFor='input-file' className='h-full'>
-                <div className='px-2 text-nowrap flex items-center bg-cyan-400/20 shadow-lg backdrop-blur hover:brightness-125 transition font-semibold text-white first-letter:uppercase h-full rounded-full' role='button'>Select file</div>
-                <input type='file' id='input-file' multiple accept='image/jpg, image/png, image/jpeg' onChange={handleChange} className='hidden' />
-              </label>
-            </>
-            <button disabled={!files || loading} onClick={handleConvert} className='bg-indigo-400/20 shadow-lg transition backdrop-blur disabled:brightness-50 text-white font-semibold hover:brightness-125 capitalize h-full rounded-full px-2'>convert</button>
+    <div className='h-screen max-h-screen bg-gradient-to-b from-sky-400 to-sky-800'>
+      <div className='flex flex-col h-full'>
+        <header className='text-white h-12 flex flex-row justify-center items-center bg-white/30 backdrop-blur'>
+          <Link href='/' className='text-white font-bold text-2xl'>
+            Image to WEBP
+          </Link>
+        </header>
+        <main className='flex flex-col flex-1 justify-center max-w-3xl mx-auto gap-2'>
+          <h1 className='text-4xl font-bold text-center text-white'>Image to WEBP</h1>
+          <h2 className='text-center text-gray-100'>Convert your image to webp format</h2>
+          <div className='flex flex-row gap-2'>
+            <p className='w-full bg-white/20 backdrop-blur shadow-lg rounded-full overflow-hidden px-4 text-white font-light italic flex items-center flex-auto'>
+              <span className='overflow-x-scroll text-nowrap inline-block scroll-smooth w-full py-2'>{file && file.map(item => item.origin.name).join(", ") || 'Filename'}</span>
+            </p>
+            <label htmlFor='input-file' className='h-full flex-none w-fit'>
+              <div className='px-2 text-nowrap flex items-center bg-cyan-400/20 shadow-lg backdrop-blur hover:brightness-125 transition font-semibold text-white first-letter:uppercase h-full rounded-full' role='button'>Select file</div>
+              <input type='file' id='input-file' multiple accept='image/jpg, image/png, image/jpeg' onChange={handleChange} className='hidden' />
+            </label>
+            <button disabled={!files || loading} onClick={handleConvert} className='bg-indigo-400/20 flex-none w-fit shadow-lg transition backdrop-blur disabled:brightness-50 text-white font-semibold hover:brightness-125 capitalize h-full rounded-full px-2'>convert</button>
           </div>
-          <div className='min-h-56 max-h-56 h-fit w-full bg-slate-400/20 shadow-lg rounded-lg backdrop-blur flex flex-col overflow-y-scroll gap-2'>
+          <section className='flex flex-col max-h-[400px] overflow-y-auto p-3 border border-dashed rounded-lg divide-y-2 gap-1 divide-dotted'>
             {file && file.map((item) => <ImageElement key={item.origin.name} origin={item.origin} converted={item.converted} loading={loading} />)}
-          </div>
-        </section>
-      </main>
-      <footer className='text-white text-center h-8'>
-        <a href='https://twozer00.dev' className='underline'>
-          TwoZer00 &copy; {new Date().getFullYear()}
-        </a>
-      </footer>
+          </section>
+          <p className='flex flex-col'>
+            <small className='text-center text-white'>Convert file/s may take some time, server stops when it's not being used, please be patient.</small>
+            <small className='text-center text-white'>No files are being storaged. </small>
+          </p>
+        </main>
+        <footer className='text-white text-center h-8'>
+          <a href='https://twozer00.dev' className='underline'>
+            TwoZer00 &copy; {new Date().getFullYear()}
+          </a>
+        </footer>
+      </div>
     </div>
   )
 }
