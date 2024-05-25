ad3d4006df340e4b8ac47a66783471574437cf89
Leobardo Verdugo <leobardo@twozer00.dev>
2024-05-25 02:08:38 -0700

    fix upload file over 5mb

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,8 @@
 import { PropTypes } from 'prop-types'
 import { useRef, useState } from 'react'
-import { Box, Stack, Button, CssBaseline, ThemeProvider, createTheme, Typography, IconButton, useTheme, alpha, LinearProgress, Chip, TextField, InputAdornment } from '@mui/material'
+import { Box, Stack, Button, CssBaseline, ThemeProvider, createTheme, Typography, IconButton, useTheme, alpha, LinearProgress, Chip, TextField, InputAdornment, Tooltip } from '@mui/material'
 import Masonry from '@mui/lab/Masonry'
-import { Close, Delete, Download } from '@mui/icons-material'
+import { Close, Compare, Delete, Download, Upload } from '@mui/icons-material'
 const API_URL = 'https://image-converter-k56z.onrender.com/api/image/converter'
 export default function Main () {
   const theme = createTheme()
@@ -21,10 +21,23 @@ export default function Main () {
         return // work as continue
       }
       item.loading = true
-      fetch(API_URL, { method: 'POST', body: formData }).then((res) => res.blob()).then((blob) => {
+      fetch(API_URL, { method: 'POST', body: formData }).then(async (res) => {
+        if (res.status !== 200) {
+          throw new Error((await res.json()).message)
+        }
+        return res.blob()
+      }).then((blob) => {
         item.fileconverted = blob // save blob to item
         item.downloable = true
         item.loading = false
+        setFiles((val) => {
+          const temp = [...val]
+          temp[index] = item
+          return temp
+        })
+      }).catch((error) => {
+        item.loading = false
+        item.error = error.message
         setFiles((val) => {
           const temp = [...val]
           temp[index] = item
@@ -50,22 +63,7 @@ const InputFilename = ({ file, index }) => {
   const [data, setFiles] = file
   const inputRef = useRef(null)
   const [props, setProps] = useState({
-    readOnly: true,
-    endAdornment:
-  <InputAdornment position='end'>
-    <IconButton
-      size='small' onClick={() => {
-        inputRef.current.value = ''
-        setFiles((val) => {
-          const temp = [...val]
-          delete temp[index].newName
-          return temp
-        })
-      }}
-    >
-      <Close />
-    </IconButton>
-  </InputAdornment>
+    readOnly: true
   })
   const handleChange = (e) => {
     setFiles((val) => {
@@ -76,8 +74,52 @@ const InputFilename = ({ file, index }) => {
   }
   return (
     <>
-      <TextField InputProps={props} inputProps={{ ref: inputRef, autocomplete: 'off' }} onClick={(e) => { e.target.disabled = false }} defaultValue={data.newName} placeholder={data.name} onChange={handleChange} variant='standard' fullWidth onBlur={() => setProps({ ...props, readOnly: true })} onFocus={() => setProps({ ...props, readOnly: false })} />
+      <TextField
+        fullWidth
+        sx={{ '& .MuiInput-root:before': { border: 'none' }, '& .MuiInput-root:hover:before': { border: 'none' } }}
+        InputProps={props}
+        inputProps={{ ref: inputRef, autoComplete: 'off' }}
+        defaultValue={data.newName}
+        placeholder={data.name}
+        onChange={handleChange}
+        variant='standard'
+        onBlur={() => {
+          setProps((val) => {
+            const temp = { ...val }
+            temp.readOnly = true
+            delete temp.endAdornment
+            return temp
+          })
+        }}
+        onFocus={() => {
+          setProps((val) => {
+            const temp = { ...val }
+            temp.readOnly = false
+            temp.endAdornment = <CloseCustomButton inputRef={inputRef} setFiles={setFiles} index={index} />
+            return temp
+          })
+        }}
+      />
     </>
+  )
+}
+
+const CloseCustomButton = (props) => {
+  return (
+    <InputAdornment position='end'>
+      <IconButton
+        size='small' onClick={() => {
+          props.inputRef.current.value = ''
+          props.setFiles((val) => {
+            const temp = [...val]
+            delete temp[props.index].newName
+            return temp
+          })
+        }}
+      >
+        <Close />
+      </IconButton>
+    </InputAdornment>
   )
 }
 
@@ -92,6 +134,10 @@ const InputFile = ({ files, converter }) => {
       setData(temp.slice(0, 5))
       return alert('max 5 file at same time')
     }
+    if (temp.some(item => getSizeNumber(item.size) >= 5)) {
+      setData(temp.filter(item => getSizeNumber(item.size) < 5))
+      return alert('file size must not be greater than 5MB')
+    }
     setData(temp)
   }
   const handleClear = () => {
@@ -103,7 +149,7 @@ const InputFile = ({ files, converter }) => {
       <Stack direction='row' gap={1}>
         <input accept='image/png,image/jpg,image/jpeg' type='file' multiple hidden id='input-file' onChange={handleChange} ref={fileInputRef} />
         <Stack component='label' htmlFor='input-file'>
-          <Button variant='contained' sx={{ textWrap: 'nowrap', width: 'fit-content' }} component='span' role='button'>
+          <Button startIcon={<Upload />} variant='contained' sx={{ textWrap: 'nowrap', width: 'fit-content' }} component='span' role='button'>
             Load Files
           </Button>
         </Stack>
@@ -112,7 +158,7 @@ const InputFile = ({ files, converter }) => {
         >
           Clear all
         </Button>
-        <Button variant='contained' sx={{ width: 'fit-content' }} disabled={data.length === 0} onClick={() => { converter() }}>
+        <Button startIcon={<Compare />} variant='contained' sx={{ width: 'fit-content' }} disabled={data.length === 0} onClick={() => { converter() }}>
           Convert {data.length > 1 && 'all'}
         </Button>
       </Stack>
@@ -135,14 +181,16 @@ const FileList = ({ files }) => {
       <Masonry columns={2} spacing={4} sx={{ maxWidth: 'xl', width: '100%', mx: 'auto', p: 1 }}>
         {data.map((file, index) => (
           <Box key={index} width='200px' border={1} p={2} boxSizing='content-box' borderRadius={2} borderColor={theme.palette.divider}>
-            <Stack direction='row' alignItems='center'>
+            <Stack direction='row' alignItems='center' mb={1}>
               <InputFilename file={[file, setData]} index={index} placeholder={file.name || 'no filename'} />
             </Stack>
             <Box position='relative'>
               <img src={URL.createObjectURL(file.fileconverted || file)} alt={file.name} style={{ height: 'auto', width: '100%', objectFit: 'contain' }} />
               {file.fileconverted && <Chip sx={{ m: 2, position: 'absolute', right: 0, top: 0, zIndex: 1, textTransform: 'uppercase' }} color='secondary' size='small' variant='contained' label='webm' />}
             </Box>
-            <Typography variant='caption' textAlign='center' display='block'>{getSize(file.size)}</Typography>
+            <Tooltip title={`${getSizeNumber(file.size) > 5 ? 'File exceeds 5Mb' : ''}`}>
+              <Typography variant='caption' color={`${getSizeNumber(file.size) > 5 ? 'crimson' : 'inherit'}`} textAlign='center' display='block'>{getSize(file.size)}</Typography>
+            </Tooltip>
             <Stack direction='row' justifyContent='flex-end' alignItems='center'>
               <IconButton disabled={!file.downloable} size='small' component='a' href={file.fileconverted && URL.createObjectURL(file.fileconverted)} download={file.newName?.replace(' ', '-') || true}>
                 <Download />
@@ -151,6 +199,7 @@ const FileList = ({ files }) => {
                 <Delete />
               </IconButton>
             </Stack>
+            {file.error && <Typography variant='subtitle1' color='crimson' textAlign='center' display='block'>{file.error}</Typography>}
           </Box>
         ))}
       </Masonry>
@@ -169,8 +218,19 @@ InputFilename.propTypes = {
   file: PropTypes.array.isRequired,
   index: PropTypes.number.isRequired
 }
+CloseCustomButton.propTypes = {
+  inputRef: PropTypes.object.isRequired,
+  setFiles: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired
+}
 const getSize = (size) => {
   const kb = size / 1024
   const mb = kb / 1024
   return (mb > 1) ? `${mb.toFixed(2)} Mb` : `${kb.toFixed(2)} Kb`
+}
+
+const getSizeNumber = (size) => {
+  const kb = size / 1024
+  const mb = kb / 1024
+  return mb.toFixed(2)
 }
