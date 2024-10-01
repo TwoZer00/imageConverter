acdc05c6af90c48c7150a02a3cd01e126632b6ca
twozer00 <leobardo.21@outlook.es>
2024-09-30 23:21:29 -0700

    add ads

---
+++ src/components/GoogleAds.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+
+const GoogleAd = () => {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({})
+    } catch (e) {
+      console.error('Error loading Google ad:', e)
+    }
+  }, [])
+
+  return (
+    <ins
+      className='adsbygoogle'
+      style={{ display: 'block', height: 'auto', width: '100%' }}
+      data-ad-client='ca-pub-7731037445831235'
+      data-ad-slot='5292510382'
+    />
+  )
+}
+
+export default GoogleAd
