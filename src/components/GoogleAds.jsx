b3aa2ceed3da133335df02a18eadf0b693fb0d13
Leobardo Verdugo <leobardo@twozer00.dev>
2024-10-01 10:56:18 -0700

    ads fix

--- src/components/GoogleAds.jsx
+++ src/components/GoogleAds.jsx
@@ -13,7 +13,6 @@ const GoogleAds = () => {
     <ins
       className='adsbygoogle'
       style={{ display: 'inline-block', width: '100%', height: 'auto' }}
-      data-ad-format='auto'
       data-ad-client='ca-pub-7731037445831235'
       data-ad-slot='5292510382'
     />
